48fff6dbb1bd06510adf329acc5c1fb665a743d6
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-08-20 12:36:52 -0400

    Ejercicio 6 hecho

---
+++ trabajosoMatematica/Ejercicio_6.py
@@ -0,0 +1,22 @@
+def campañasPublicitariasTotal(vistas : int):
+
+    return (vistas - 500) / 300
+
+def visitasTotales(campañas : int):
+
+    return campañas * 300 + 500
+
+# V(x) = 300 * x + 500
+
+# 1) La variable dependiente de este problmea es la cantidad de visitas totales | La variable independiente son la cantidad de campañas hechas.
+
+# 2) El dominio de esta funcion con el caso hipotetico es el siguiente : x = [0, 20]; con x siendo un numero entero.
+
+# 3) La pendiente de la funcion es 300, esto significa que por cada campaña publicitaria hecha, el total de visitas aumenta 300.
+
+# 4) Si se hacen 5 campañas publicitarias, el total de visitas sera de 2000.
+
+# 5)
+
+print(visitasTotales(5))
+print(campañasPublicitariasTotal(4700))
