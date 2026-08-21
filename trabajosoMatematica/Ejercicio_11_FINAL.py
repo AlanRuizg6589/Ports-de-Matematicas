59e9f2d16fbce13bc3492de2fd849e66b0a4bc94
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-08-21 18:15:02 -0400

    FINAL LO TERMINE HERMANO HOLY SHIT

---
+++ trabajosoMatematica/Ejercicio_11_FINAL.py
@@ -0,0 +1,15 @@
+# C₁(x) = 12 * x + 50
+
+# C₂(z) = 8 * z + 100
+
+def semanasDataCenter_1(costo : float):
+    
+    return (costo - 50) / 12
+
+print(semanasDataCenter_1(155))
+
+# 1) (Screenshot tomada! ! !)
+
+# 2) Si el costo del data center 1 es de 155 millones de peso, entonces transcurrio 8.75 semanas.
+
+# 3) Depende completamente de cuanto dure el proyecto, en el primer año, el proyecto 1 es mucho mas rentable, pero desde el segundo año, el proyecto 2 sale mucho menos costoso que el proyecto 1.
