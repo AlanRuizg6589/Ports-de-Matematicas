f408f18796a558555176eb8eecdb85d2c4a2c73a
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-08-30 20:40:35 -0400

    M

---
+++ Matematicosas/Ejercicio_11.py
@@ -0,0 +1,3 @@
+# 1) La función f(x) esta divida en dos partes distintas dependiendo del valor que tenga la x. Si x < 1, la función secundaria que se usara es 2 - x. Y si x es >= 1, entonces la función que representara sera 3. Independiente del valor de x (mientras sea mayor o igual a 1) el valor de f(x) sera 3.
+
+# 2) Si x = -5, entonces f(x) = 7 | Si x = 5, f(x) = 3
