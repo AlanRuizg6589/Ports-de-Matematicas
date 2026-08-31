a049ec64b4d0f17fd16f990ce494d9c60e2b6207
PM- Alumno <PM-Alumno@duoc.acad>
2026-08-31 16:29:53 -0400

    wow

---
+++ Matematicosas/Ejercicio_8.py
@@ -0,0 +1,15 @@
+# A(x) = (600 * (1.05 ** x))
+
+# O(x) = (500 * (1.07 ** x))
+
+# 1) La forma del sueldo mensual de la oferta recibida se representa de las siguiente forma: O(x) = (500 * (1.07 ** x))
+
+# 2) Screenshot tomada!!!
+
+# 3) Le recomendaría que tome la oferta de trabajo que le dieron. Con su oferta de trabajo empieza a ganar más despues de vender más de 10 casas. Y la diferencia de antes es poco notable para considerarla.
+
+#
+
+#
+
+#
