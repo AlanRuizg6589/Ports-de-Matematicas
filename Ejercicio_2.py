eb1b817e523f3a77c843d60b6e978caf61dd38aa
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-09-08 11:44:38 -0300

    ow

---
+++ Ejercicio_2.py
@@ -0,0 +1,24 @@
+
+def sucesión(n : int):
+    
+    return 5 * (n ** 3)
+
+def sucesión_inversa(y : float):
+    
+    return (y / 5) ** (1/3)
+ 
+#for i in range(4):
+    
+    print(sucesión(i + 1))
+    
+#for i in range(5):
+    
+    print(sucesión(i + 9))
+
+print(sucesión_inversa(40000))
+
+# 1.- 5, 40, 135, 320
+
+# 2.- 3645, 5000, 6655, 8640
+
+# 3.- Si, pertenece a la sucesión, siendo el valor 20. 
