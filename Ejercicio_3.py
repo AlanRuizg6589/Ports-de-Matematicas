7917b4f550b91fefa3bfd1c9ccc3887bdc777ea6
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-09-08 20:20:15 -0300

    Ejercicio 3

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -13,7 +13,9 @@ def nueva_sucesión(número : int):
 
     Patrones["F_" + str(número)] = Patrones["F_" + str(número - 1)] + Patrones["F_" + str(número - 2)]
 
-for i in range (0, 21):
+# Respuesta de la pregunta 1:
+
+for i in range (0, 31):
 
     if i in [0, 1]:
 
@@ -21,6 +23,18 @@ for i in range (0, 21):
 
     nueva_sucesión(i)
 
+    if i >= 21:
+        
+        continue
+    
     print(f"F_{i} : {Patrones["F_" + str(i)]}")
 
-print(Patrones)
+Suma_total = 0
+
+# Respuesta de la pregunta 2:
+
+for número in range (31):
+    
+    Suma_total += Patrones["F_" + str(número)]
+    
+    print(Suma_total)
