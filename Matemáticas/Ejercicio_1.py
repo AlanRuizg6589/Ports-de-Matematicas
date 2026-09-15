95e82d67ea8335504262d9b09a0fc11233dc5abd
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-09-15 14:06:23 -0300

    talento

---
+++ Matemáticas/Ejercicio_1.py
@@ -0,0 +1,22 @@
+
+def GB(Costo_total : int):
+
+    return (Costo_total - 4200) / 1650
+
+def costoTotal(GB : int):
+
+    return (1650 * GB) + 4200
+
+print(costoTotal(48))
+
+print(GB(103200))
+
+# a) La variable independiente de la función es la cantidad de GB transmitidos | La variable dependiente de la función es el costo total de el servicio.
+
+# b) El dominio de la función es el siguiente: x = [0, Infinito positivo[
+
+# c) La pendiente de la función es 1650, e indica el valor de cada GB que se transmite.
+
+# d) El costo toal de trasnmitir 84 GB es de $83400.
+
+# e) Si el cosot final es de $103.200, entonces la cantidad de GB transmitida es de 60 GB.
---
+++ Matemáticas/Ejercicio_2.py
@@ -0,0 +1,14 @@
+A = (3, 85) y B = (7, 45)
+
+# a) La variable dependiente de la función es la cantidad de combustible vaciado | La variable independiente es la cantidad de tiempo que transcurre (medidos en minutos)
+
+# b) La pendiente de la función es -10.
+
+# c) La pendiente indica la cantidad vaciada de combustible por minuto.
+
+# d) La función algebraica de el ejercicio se escribe de la siguiente forma: y = -10 * x + 115
+
+# e) El dominio de la funcíon se escribe de la siguiente manera: x = [0, 11,5]
+#    x indica la cantidad de minutos que trasncurren, el momento que llega al minuto 11 segundo 30, la cantidad de combustible que se puede tirar se acaba.
+
+# f) Si transcurren 10 minutos, la cantidad de combustible que queda es de 15 L.
